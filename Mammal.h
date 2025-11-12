347f37ca6b238d3169e4b403b30f01384808de2a
Mohamed-Mokhtar-Elalfy-16009527 <mohamedshaker25000@gmail.com>
2025-11-12 15:15:11 +0200

    Update Mammal.h

--- Mammal.h
+++ Mammal.h
@@ -5,7 +5,16 @@
 
 //define mammal class here
 
-
+class Mammal:public Animal{
+private:
+    string furColor;
+public:
+    Mammal();
+    ~Mammal();
+    Mammal(string name,int age,bool isHungry,string furColor);
+    string getfurColor();
+    void setfurColor(string r);
+};
 
 
 
