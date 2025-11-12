404f69be9a8bcac0e60078c8e78d2a038804e7d5
Mohamed-Mokhtar-Elalfy-16009527 <mohamedshaker25000@gmail.com>
2025-11-12 15:14:04 +0200

    Update Bird.h

--- Bird.h
+++ Bird.h
@@ -5,7 +5,16 @@
 
 //define class bird here 
 
-
+class Bird:public Animal{
+private:
+    float wingspan;
+public:
+    Bird();
+    ~Bird();
+    Bird(string name,int age,bool isHungry,float wingspan);
+    float getwingspan();
+    void setwingspan(float r);
+};
 
 
 
